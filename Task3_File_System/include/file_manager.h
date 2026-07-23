2d02293a609b6ee9903b29cc49954e5ef30eeda8
Dipesh Yonjan <ydpesh7@gmail.com>
2026-07-24 02:05:41 +0545

    Add file system interface definitions

---
+++ Task3_File_System/include/file_manager.h
@@ -0,0 +1,16 @@
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
+
+/* Create a new file */
+void create_file();
+
+/* Write text into a file */
+void write_file();
+
+/* Read file contents */
+void read_file();
+
+/* Delete a file */
+void delete_file();
+
+#endif
---
+++ Task3_File_System/include/permissions.h
@@ -0,0 +1,10 @@
+#ifndef PERMISSIONS_H
+#define PERMISSIONS_H
+
+/* Display file permissions */
+void show_permissions();
+
+/* Display file information */
+void show_file_info();
+
+#endif
