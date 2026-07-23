8aa0cc11eb6d138baa22b3437210eefd75d2900b
Dipesh Yonjan <ydpesh7@gmail.com>
2026-07-24 02:24:50 +0545

    Add Task 4 networking and IPC interfaces

---
+++ Task4_Network_IPC/include/ipc_manager.h
@@ -0,0 +1,7 @@
+#ifndef IPC_MANAGER_H
+#define IPC_MANAGER_H
+
+/* Demonstrate pipe-based IPC */
+void pipe_demo();
+
+#endif
---
+++ Task4_Network_IPC/include/socket_manager.h
@@ -0,0 +1,7 @@
+#ifndef SOCKET_MANAGER_H
+#define SOCKET_MANAGER_H
+
+/* Simulate socket communication */
+void socket_demo();
+
+#endif
